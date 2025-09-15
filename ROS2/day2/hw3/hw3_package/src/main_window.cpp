ff93b2d5209c8ae55976d525a544bec743a41de7
Changseok_Hyun <143160568+sasdfdse@users.noreply.github.com>
2025-09-15 14:38:04 +0900

    Update main_window.cpp

--- ROS2/day2/hw3/hw3_package/src/main_window.cpp
+++ ROS2/day2/hw3/hw3_package/src/main_window.cpp
@@ -21,15 +21,10 @@ void MainWindow::on_pushButton_clicked()
     static QVector<QString> queue_v;   
     queue_v.prepend(ui->lineEdit->text());
 
-
-
-
     if (!queue_v.isEmpty()) {
         QString first_qstr = queue_v[0];
 
-
         std::string tostring = first_qstr.toStdString();
-
 
         std_msgs::msg::String msg;
         msg.data = tostring;
@@ -38,7 +33,5 @@ void MainWindow::on_pushButton_clicked()
 
         ui->lineEdit->clear();
         ui->log->append(QString("[Publish] ") + QString::fromStdString(tostring));
-
     }
-
 }
